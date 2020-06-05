24f487a1db87748a92a7e1fd3822b4afd6e9dc7e
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2020-06-05 14:36:11 +0100

    Initial commit

---
+++ SoundexTest.cpp
@@ -0,0 +1,22 @@
+#include <string>
+
+class Soundex
+{
+    public:
+       std::string encode(const std::string& word) const
+       {
+          return "";
+       }
+};
+
+
+#include "gmock/gmock.h"
+
+TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord)
+{
+   Soundex soundex;
+
+   auto encoded = soundex.encode("A");
+
+}
+
